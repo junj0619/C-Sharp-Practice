b5ac343ff1fa5a51f8ba7224edd21bf3fd9d0f42
Jun <Jun@JUN-PC>
2017-01-03 22:26:43 -0500

    Add project files.

---
+++ Testability/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Testability
+{
+    public class Order
+    {
+        public float TotalPrice { get; set; }
+        public bool IsShipped { get; set; }
+        public Shipment Shipment { get; set; }
+    }
+}
---
+++ Testability/OrderProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace Testability
+{
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed.");
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
