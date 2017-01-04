481389e1ef3d208093c563c0c10a7eea072ec93f
Jun <Jun@JUN-PC>
2017-01-03 23:20:36 -0500

    Apply Unit Test On OrderProcessor

--- Testability/Order.cs
+++ Testability/Order.cs
@@ -4,9 +4,19 @@ namespace Testability
 {
     public class Order
     {
+        public int Id { get; set; }
         public DateTime DatePlaced { get; set; }
         public float TotalPrice { get; set; }
-        public bool IsShipped { get; set; }
+
+        public bool IsShipped
+        {
+            get
+            {
+                return Shipment != null;
+
+            }
+        }
+
         public Shipment Shipment { get; set; }
     }
 }
--- Testability/OrderProcessor.cs
+++ Testability/OrderProcessor.cs
@@ -5,11 +5,11 @@ namespace Testability
 {
     public class OrderProcessor
     {
-        private readonly ShippingCalculator _shippingCalculator;
+        private readonly IShippingCalculator _shippingCalculator;
 
-        public OrderProcessor()
+        public OrderProcessor(IShippingCalculator shippingCalculator)
         {
-            _shippingCalculator = new ShippingCalculator();
+            _shippingCalculator = shippingCalculator;
         }
 
         public void Process(Order order)
